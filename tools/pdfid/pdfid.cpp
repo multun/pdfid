0623d539384d07b6507e1155bfe4f10a516aaff0
Victor multun Collod <victor.collod@epita.fr>
2019-11-11 18:13:30 +0100

    pdfid: improve command line API

--- tools/pdfid/pdfid.cpp
+++ tools/pdfid/pdfid.cpp
@@ -4,203 +4,191 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
-using std::cerr;
-using std::cout;
-using std::cin;
-using std::flush;
-using std::endl;
-using std::string;
+#include <cstring>
 
 using namespace PoDoFo;
 
-enum PdfIDMode
+int open_output_file(std::ofstream &file, const char *path)
 {
-    PDFID_READ,
-    PDFID_WRITE,
-};
-
-struct PdfIDOptions
-{
-    EPdfWriteMode eWriteMode;
-    bool useDemandLoading;
-    bool useStrictMode;
-    const char* input_filename;
-    const char* output_filename;
-    const char* input_data;
-    const char* output_data;
-    enum PdfIDMode mode;
-
-    PdfIDOptions()
-        : eWriteMode(ePdfWriteMode_Default)
-        , useDemandLoading(false)
-        , useStrictMode(false)
-        , input_filename(nullptr)
-        , output_filename(nullptr)
-        , input_data(nullptr)
-        , output_data(nullptr)
-        , mode(PDFID_READ)
-    {}
-
-    static int usage(int rc)
-    {
-        cerr << ("Usage: pdfid [-d] [-s] [-clean] [-compact] [-r <output_data>]"
-                 " [-w <input_data>] <input_filename> <output_filename>\n")
-             << "    -d       Enable demand loading of objects\n"
-             << "    -s       Enable strict parsing mode\n"
-             << "    -clean   Enable clean writing mode\n"
-             << "    -compact Enable compact writing mode\n"
-             << "    -r       Read data from the pdf file into output_data\n"
-             << "    -w       Write input_data into the pdf file\n"
-             << flush;
-        return rc;
-    }
-
-    // worst argument parsing ever
-    int parse(int argc, char *argv[])
-    {
-        for (int i = 1; i < argc; i++)
-        {
-            if (argv[i][0] == '-')
-            {
-                if (string("-d") == argv[i])
-                    useDemandLoading = true;
-                else if (string("-s") == argv[1])
-                    useStrictMode = true;
-                else if (string("-clean") == argv[i])
-                    eWriteMode = ePdfWriteMode_Clean;
-                else if (string("-compact") == argv[i])
-                    eWriteMode = ePdfWriteMode_Compact;
-                else if (string("-r") == argv[i])
-                {
-                    mode = PDFID_READ;
-                    if ((output_data = argv[i++ + 1]) == nullptr)
-                    {
-                        std::cerr << "Missing input file" << std::endl;
-                        return usage(1);
-                    }
-                }
-                else if (string("-w") == argv[i])
-                {
-                    mode = PDFID_WRITE;
-                    if ((input_data = argv[i++ + 1]) == nullptr)
-                    {
-                        std::cerr << "Missing output file" << std::endl;
-                        return usage(1);
-                    }
-
-                }
-                continue;
-            }
-
-            if (input_filename == NULL)
-                input_filename = argv[i];
-            else if (output_filename == NULL)
-                output_filename = argv[i];
-            else
-            {
-                cerr << "unknown positional argument: " << argv[i] << endl;
-                return 1;
-            }
-        }
-
-        switch (mode)
-        {
-        case PDFID_READ:
-            if (input_filename != nullptr && output_filename == nullptr && output_data != nullptr)
-                return 0;
-            break;
-        case PDFID_WRITE:
-            if (input_filename != nullptr && output_filename != nullptr)
-                return 0;
-            break;
-        }
-        return usage(1);
-    }
-};
-
-int pdfid_read(PdfParser &parser, PdfIDOptions &options)
-{
-    std::ofstream output_data_stream;
     try {
-        output_data_stream.exceptions(std::ofstream::failbit | std::ofstream::badbit);
-        output_data_stream.open(options.output_data, std::ofstream::out | std::ofstream::trunc);
-    } catch (std::ofstream::failure e) {
-        std::cerr << "failed to open file: " << e.what() << std::endl;
+        file.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+        file.open(path, std::ofstream::out | std::ofstream::trunc);
+        return 0;
+    } catch (const std::ofstream::failure &e) {
+        std::cerr << "failed to open file `"
+                  << path << "': "
+                  << strerror(errno) << std::endl;
         return 1;
     }
+}
+
+int open_input_file(std::ifstream &file, const char *path)
+{
+    file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try {
+        file.open(path, std::ifstream::in);
+        file.exceptions(std::ifstream::badbit);
+        return 0;
+    } catch (const std::ifstream::failure &e) {
+        std::cerr << "failed to open file `"
+                  << path << "': "
+                  << strerror(errno) << std::endl;
+        return 1;
+    }
+}
+
+void pdfid_read_help(const char *program_name, std::ostream &o)
+{
+    o << "Usage: " << program_name
+      << " read <input_pdf> [<output_data_file>]" << std::endl;
+}
+
+int pdfid_read_stream(PdfParser &parser, std::ostream &output_data_stream, const char *input_filename)
+{
+    int rc;
 
     bit_ostream o_bitstream(output_data_stream);
-    parser.SetStrictParsing(options.useStrictMode);
-    PdfRefCountedInputDevice device( options.input_filename, "rb" );
-    if( !device.Device() )
-    {
-        std::cerr << "Couldn't open input file" << std::endl;
-        return 1;
-    }
+    parser.SetStrictParsing(false);
+
+    std::ifstream input_file_stream;
+    if ((rc = open_input_file(input_file_stream, input_filename)))
+        return rc;
+
+    PdfRefCountedInputDevice device(new PdfInputDevice(&input_file_stream));
     device.Device()->dictdecode_stream = &o_bitstream;
-    parser.ParseFile(device, options.useDemandLoading);
+    parser.ParseFile(device, false);
     o_bitstream.flush();
     return 0;
 }
 
-int pdfid_write(PdfParser &parser, PdfIDOptions &options)
-{
-    // setup the input data stream
-    std::ifstream input_data_stream;
-    input_data_stream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    try {
-        input_data_stream.open(options.input_data, std::ifstream::in);
-        input_data_stream.exceptions(std::ifstream::badbit);
-    } catch (std::ifstream::failure e) {
-        std::cerr << "failed to open file: " << e.what() << std::endl;
-        return 1;
-    }
-    bit_istream i_bitstream(input_data_stream);
-
-    // read the file
-    parser.SetStrictParsing(options.useStrictMode);
-    parser.ParseFile(options.input_filename, options.useDemandLoading);
-
-    // write it back
-    PdfWriter writer( &parser );
-    writer.SetWriteMode( options.eWriteMode );
-    writer.SetPdfVersion( ePdfVersion_1_6 );
-    PdfOutputDevice device( options.output_filename );
-    device.dictencode_stream = &i_bitstream;
-    writer.Write( &device );
-    return 0;
-}
-
-int main( int argc, char*  argv[] )
+int pdfid_read(PdfParser &parser, const char *program_name, int argc, char *argv[])
 {
     int rc;
 
-    PdfIDOptions options;
-    if ((rc = options.parse(argc, argv)))
+    if (argc != 2 && argc != 3) {
+        pdfid_read_help(program_name, std::cerr);
+        return 1;
+    }
+
+    const char *input_file = argv[1];
+    const char *output_data_file = argv[2];
+
+    // if there's no output file, write to stdout
+    if (argc == 2)
+        return pdfid_read_stream(parser, std::cout, input_file);
+
+    std::ofstream output_data_stream;
+    if ((rc = open_output_file(output_data_stream, output_data_file)))
         return rc;
+
+    return pdfid_read_stream(parser, output_data_stream, input_file);
+}
+
+void pdfid_write_help(const char *program_name, std::ostream &o)
+{
+    o << "Usage: " << program_name
+      << " write <input_pdf> <output_pdf> [<input_data>]" << std::endl;
+}
+
+int pdfid_write_stream(PdfParser &parser, std::istream &input_data_stream, const char *input_pdf_path, const char *output_pdf_path)
+{
+    int rc;
+
+    bit_istream i_bitstream(input_data_stream);
+
+    // read the file
+    parser.SetStrictParsing(false);
+    parser.ParseFile(input_pdf_path, false);
+
+    // write it back
+    PdfWriter writer(&parser);
+    writer.SetWriteMode(ePdfWriteMode_Compact);
+    writer.SetPdfVersion(ePdfVersion_1_6);
+
+    std::ofstream output_pdf_stream;
+    if ((rc = open_output_file(output_pdf_stream, output_pdf_path)))
+        return rc;
+
+    PdfOutputDevice device(&output_pdf_stream);
+    device.dictencode_stream = &i_bitstream;
+    writer.Write(&device);
+    return 0;
+}
+
+
+int pdfid_write(PdfParser &parser, const char *program_name, int argc, char *argv[])
+{
+    int rc;
+
+    if (argc != 3 && argc != 4) {
+        pdfid_write_help(program_name, std::cerr);
+        return 1;
+    }
+
+    const char *input_pdf_path = argv[1];
+    const char *output_pdf_path = argv[2];
+    const char *input_data_path = argv[3];
+
+    if (argc == 3)
+        return pdfid_write_stream(parser, std::cin, input_pdf_path, output_pdf_path);
+
+    // setup the input data stream
+    std::ifstream input_data_stream;
+    if ((rc = open_input_file(input_data_stream, input_data_path)))
+        return rc;
+
+    return pdfid_write_stream(parser, input_data_stream, input_pdf_path, output_pdf_path);
+}
+
+struct PdfIDSubcommand {
+    int (*command)(PdfParser &parser, const char *program_name, int argc, char *argv[]);
+    void (*help)(const char *program_name, std::ostream &o);
+    const char *name;
+};
+
+PdfIDSubcommand subcommands[] = {
+    {
+        .name = "write",
+        .command = pdfid_write,
+        .help = pdfid_write_help
+    },
+    {
+        .name = "read",
+        .command = pdfid_read,
+        .help = pdfid_read_help
+    },
+};
+
+void help(const char *program_name, std::ostream &o)
+{
+    for (const auto& subcommand : subcommands)
+        subcommand.help(program_name, o);
+}
+
+int main(int argc, char* argv[])
+{
+    int rc;
 
     PdfError::EnableLogging(true);
     PdfError::EnableDebug(true);
 
     PdfVecObjects objects;
     PdfParser parser(&objects);
-
     objects.SetAutoDelete(true);
 
-    try {
-        switch (options.mode)
-        {
-        case PDFID_READ:
-            return pdfid_read(parser, options);
-        case PDFID_WRITE:
-            return pdfid_write(parser, options);
-        }
-    } catch (PdfError & e) {
-        e.PrintErrorMsg();
-        return e.GetError();
+    if (argc < 2) {
+        help(argv[0], std::cerr);
+        return 1;
     }
 
-    cerr << "Parsed and wrote successfully" << endl;
-    return 0;
+    argc -= 1;
+    argv += 1;
+
+    const char *subcommand_name = argv[0];
+    for (const auto& subcommand : subcommands)
+        if (strcmp(subcommand.name, subcommand_name) == 0)
+            return subcommand.command(parser, argv[0], argc, argv);
+
+    help(argv[0], std::cerr);
+    return 1;
 }
